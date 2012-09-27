5b3a1db88fc023557d8b1c70b9713440c734cb60
Matt Glover <mattgloveruk@matts-macbook-2.lan>
2012-09-27 22:50:58 +0100

    Added Labels to Demo Shifted Demo checkboxes over a bit

--- DEMO/MGCheckbox/AppDelegate.m
+++ DEMO/MGCheckbox/AppDelegate.m
@@ -26,24 +26,42 @@
     self.window.backgroundColor = [UIColor whiteColor];
     
     MGCheckbox *checkboxDefault = [MGCheckbox checkbox];
-    [checkboxDefault setCenter:CGPointMake(200, 100)];
+    [checkboxDefault setCenter:CGPointMake(240, 100)];
     [checkboxDefault addTarget:self action:@selector(checkBoxValueChanged:) forControlEvents:UIControlEventTouchUpInside];
+    UILabel *checkboxDefaultLabel = [self checkboxLabelWithLabelText:@"checkboxDefault"];
+    [checkboxDefaultLabel setCenter:CGPointMake(90, checkboxDefault.center.y)];
+    [self.window addSubview:checkboxDefaultLabel];
     [self.window addSubview:checkboxDefault];
     
+    
+    
     MGCheckbox *checkboxWithStyle = [MGCheckbox checkboxWithStyle:CheckboxStyleDefault];
-    [checkboxWithStyle setCenter:CGPointMake(200, 180)];;
+    [checkboxWithStyle setCenter:CGPointMake(240, 180)];;
     [checkboxWithStyle addTarget:self action:@selector(checkBoxValueChanged:) forControlEvents:UIControlEventTouchUpInside];
+    UILabel *checkboxWithStyleLabel = [self checkboxLabelWithLabelText:@"checkboxWithStyle"];
+    [checkboxWithStyleLabel setCenter:CGPointMake(90, checkboxWithStyle.center.y)];
+    [self.window addSubview:checkboxWithStyleLabel];
     [self.window addSubview:checkboxWithStyle];
     
+    
+    
     MGCheckbox *checkboxWithImages = [MGCheckbox checkboxWithOnImage:[UIImage imageNamed:@"checkbox-on"] offImage:[UIImage imageNamed:@"checkbox-off"]];
-    [checkboxWithImages setCenter:CGPointMake(200, 260)];;
+    [checkboxWithImages setCenter:CGPointMake(240, 260)];;
     [checkboxWithImages addTarget:self action:@selector(checkBoxValueChanged:) forControlEvents:UIControlEventTouchUpInside];
+    UILabel *checkboxWithImagesLabel = [self checkboxLabelWithLabelText:@"checkboxWithImages"];
+    [checkboxWithImagesLabel setCenter:CGPointMake(90, checkboxWithImages.center.y)];
+    [self.window addSubview:checkboxWithImagesLabel];
     [self.window addSubview:checkboxWithImages];
     
+    
+    
     MGCheckbox *checkboxDefaultNoPulseOnSelect = [MGCheckbox checkbox];
-    [checkboxDefaultNoPulseOnSelect setCenter:CGPointMake(200, 340)];
+    [checkboxDefaultNoPulseOnSelect setCenter:CGPointMake(240, 340)];
     [checkboxDefaultNoPulseOnSelect setPulseOnSelect:NO];;
     [checkboxDefaultNoPulseOnSelect addTarget:self action:@selector(checkBoxValueChanged:) forControlEvents:UIControlEventTouchUpInside];
+    UILabel *checkboxDefaultNoPulseOnSelectLabel = [self checkboxLabelWithLabelText:@"checkboxDefault-NoPulse"];
+    [checkboxDefaultNoPulseOnSelectLabel setCenter:CGPointMake(90, checkboxDefaultNoPulseOnSelect.center.y)];
+    [self.window addSubview:checkboxDefaultNoPulseOnSelectLabel];
     [self.window addSubview:checkboxDefaultNoPulseOnSelect];
     
     [self.window makeKeyAndVisible];
@@ -55,31 +73,44 @@
     NSLog(@"handle checkbox value change here");
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
-@end
+// Helper Method for the Label
+- (UILabel *)checkboxLabelWithLabelText:(NSString *)labelText {
+    
+    UILabel *checkboxLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 20)] autorelease];
+     [checkboxLabel setBackgroundColor:[UIColor clearColor]];
+     [checkboxLabel setTextAlignment:UITextAlignmentRight];
+     [checkboxLabel setText:labelText];
+     [checkboxLabel setTextColor:[UIColor blackColor]];
+     [checkboxLabel setFont:[UIFont boldSystemFontOfSize:14.0]];
+     
+     return checkboxLabel;
+     }
+     
+     - (void)applicationWillResignActive:(UIApplication *)application
+    {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+     
+     - (void)applicationDidEnterBackground:(UIApplication *)application
+    {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+     
+     - (void)applicationWillEnterForeground:(UIApplication *)application
+    {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+     
+     - (void)applicationDidBecomeActive:(UIApplication *)application
+    {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+     
+     - (void)applicationWillTerminate:(UIApplication *)application
+    {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+     
+     @end
