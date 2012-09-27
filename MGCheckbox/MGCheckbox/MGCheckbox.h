e1cd734a5b12b78f9696a053ed72ad60039cf126
Matt Glover <matt@duchysoftware.com>
2012-09-27 16:15:07 +0100

    added default checkbox image resources and empty MGCheckbox class

---
+++ MGCheckbox/MGCheckbox/MGCheckbox.h
@@ -0,0 +1,13 @@
+//
+//  MGCheckbox.h
+//  MGCheckbox
+//
+//  Created by Matt Glover on 27/09/2012.
+//  Copyright (c) 2012 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGCheckbox : UIControl
+
+@end
---
+++ MGCheckbox/MGCheckbox/MGCheckbox.m
@@ -0,0 +1,31 @@
+//
+//  MGCheckbox.m
+//  MGCheckbox
+//
+//  Created by Matt Glover on 27/09/2012.
+//  Copyright (c) 2012 Duchy Software Ltd. All rights reserved.
+//
+
+#import "MGCheckbox.h"
+
+@implementation MGCheckbox
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
